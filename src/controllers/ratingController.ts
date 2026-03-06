eb81cf877aa5d523b5749f942c2297a1c3631293
caroline ackermann <caroline.ackermann@telia.com>
2026-03-06 14:27:25 +0100

    rating post is added

--- src/controllers/ratingController.ts
+++ src/controllers/ratingController.ts
@@ -11,9 +11,34 @@ interface Rating extends RowDataPacket {
   collectionId: number;
   ratedBy: string;
 }
+
 export const getRatings = async (_req: Request, res: Response) => {
   const [results] = await mysqlDatabase.query<Rating[]>(
     'SELECT * FROM ratings WHERE collectionId = 2',
+    //borde jag inte endra över så de bara står SELECT * FROM ratings?
   );
   res.status(200).json(results);
+};
+
+export const createRating = async (
+  req: Request<{ ratingScore: number; collectionId: number; ratedBy: string }>,
+  res: Response,
+) => {
+  const { ratingScore, collectionId, ratedBy } = req.body;
+  if (!ratingScore || !collectionId || !ratedBy) {
+    return res.status(400).json({ error: 'Write in all fields please' });
+  }
+  const sql =
+    'INSERT INTO ratings(ratingScore, collectionId, ratedBy) VALUES (?,?,?)';
+  try {
+    await mysqlDatabase.execute<Rating[]>(sql, [
+      ratingScore,
+      collectionId,
+      ratedBy,
+    ]);
+  } catch (err) {
+    console.error('Error creating rating:', err);
+    return res.status(500).json({ error: 'Failed to create rating' });
+  }
+  res.status(201).send('Your rating was successfully added');
 };
--- src/routes/ratingRoutes.ts
+++ src/routes/ratingRoutes.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 import { getRatings } from '../controllers/ratingController.ts';
+import { createRating } from '../controllers/ratingController.ts';
 
 router.get('/ratings', getRatings);
+router.post('/rating', createRating);
 
 export default router;
